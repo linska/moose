36ab2c4b50b15747086bdb87bd0bf58fa4f6410a
Tanya Linska <linskatanya@gmail.com>
2019-10-25 11:57:27 +0300

    task 03: updated svg & scss files

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,18 @@
 console.log("start js logic :)");
 
-function Magic() {
-    alert("wow");
+var email = document.getElementById("email");
+
+
+function onEmailFocus() {
+    document.getElementById("ani-eye_r").beginElement();
+    document.getElementById("ani-eye_l").beginElement();
+    document.getElementById("ani-pupil_r").beginElement();
 }
+
+function onEmailBlur() {
+    document.getElementById("ani-eye_rb").beginElement();
+    document.getElementById("ani-eye_lb").beginElement();
+}
+
+email.addEventListener("focus", onEmailFocus, false);
+email.addEventListener("blur", onEmailBlur, false);
